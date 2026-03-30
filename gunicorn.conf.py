f9c17a4c31499571c42abfa5901c0e80c617472f
Baymirzaev-A <andreybaymir@gmail.com>
2026-03-30 16:59:47 +0300

    вариативность в настройке количества одновременных подключений

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,10 +3,10 @@ import multiprocessing
 
 # Привязка к порту
 bind = f"0.0.0.0:{os.environ.get('PORT', 5000)}"
-
+#bind = ["0.0.0.0:5000", "0.0.0.0:5001", "0.0.0.0:5002", "0.0.0.0:5003"] - для большего количества воркеров
 # Количество workers (обычно = количество ядер CPU * 2 + 1)
 workers = int(os.environ.get('GUNICORN_WORKERS', 4))
-
+threads = int(os.environ.get('GUNICORN_THREADS', 10))
 # Worker класс для WebSocket (eventlet)
 worker_class = "eventlet"
 
